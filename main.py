7d6bae9af8e8ace6734d1c83533f813c39ef5e1f
MaybeBroken <davidsponseller123@gmail.com>
2025-04-02 10:24:29 -0600

    Add initial implementation of the application with UI and file management

---
+++ main.py
@@ -0,0 +1,342 @@
+from panda3d.core import *
+from direct.showbase.ShowBase import ShowBase
+from direct.gui.DirectGui import *
+from panda3d.core import (
+    NodePath,
+    Vec3,
+    NodePath,
+    MovieTexture,
+    CardMaker,
+    loadPrcFileData,
+    TransparencyAttrib,
+)
+from json import loads, dump
+import os
+import atexit
+import time
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+GLOBALMEM: dict = {}
+DEFAULTS: dict = {
+    "AUTH": {"users": {"admin": "admin"}},
+    "PANDAPRC": {
+        "PRCFILE": """win-size 1280 720
+window-title Windows 11
+text-font /Windows 11/scripts/src/SegoeUIVF-Regular-Small.ttf
+undecorated true
+clock-mode limited
+clock-frame-rate 0
+show-frame-rate-meter 1
+frame-rate-meter-update-interval 0.25
+sync-video 0
+""",
+        "STARTUP_INJECTOR": "print('No startup injector found.')",
+    },
+}
+
+
+class UIManager:
+    def __init__(self):
+        self.pageStack: dict[str, UIManager.Window] = {}
+        self.lastPage = None
+        self.activePage = None
+
+    def goToPage(self, pageName):
+        if pageName in self.pageStack:
+            if self.activePage is not None:
+                self.activePage.hide()
+            self.pageStack[pageName].show()
+            self.lastPage = self.activePage
+            self.activePage = self.pageStack[pageName]
+        else:
+            print(f"Page '{pageName}' Not Found")
+
+    def goBack(self):
+        if self.lastPage is not None:
+            self.activePage.hide()
+            self.lastPage.show()
+            self.activePage = self.lastPage
+            self.lastPage = None
+        else:
+            print("No Last Page Found")
+
+    def addPage(self, pageName, page):
+        if pageName not in self.pageStack:
+            self.pageStack[pageName] = page
+            if self.activePage is None:
+                self.activePage = page
+            else:
+                page.hide()
+        else:
+            print("Page Already Exists")
+
+    class VideoPlayer:
+        def __init__(self, name, videoPath, parent: "UIManager.Window" = None):
+            self.name = name
+            self.videoPath = videoPath
+            self.tex = MovieTexture(self.name)
+            if self.tex.read(videoPath):
+                self.tex.setLoop(False)
+                cm = CardMaker("fullscreenCard")
+                cm.setFrameFullscreenQuad()
+                cm.setUvRange(self.tex)
+                card = NodePath(cm.generate())
+                card.reparentTo(parent if parent is not None else render2d)
+                card.setTexture(self.tex)
+                self.card = card
+                self.play()
+            else:
+                with open(videoPath, "rb") as fp:
+                    print("File Found, " + str(len(fp.read())) + " bytes")
+                print("Failed to load Video")
+
+        def stop(self):
+            try:
+                self.tex.stop()
+            except:
+                pass
+
+        def play(self):
+            try:
+                self.tex.play()
+            except:
+                pass
+
+        def setSpeed(self, speed):
+            try:
+                self.tex.setPlayRate(speed)
+            except:
+                pass
+
+        def getSpeed(self):
+            try:
+                return self.tex.getPlayRate()
+            except:
+                return 0
+
+        def setLoop(self, loop):
+            try:
+                self.tex.setLoop(loop)
+            except:
+                pass
+
+    class Window:
+        def __init__(self, name, UIMgr: "UIManager", parent: "UIManager.Window" = None):
+            self.name = name
+            self.visible = False
+            self.parent = None if parent is None else parent
+            self.children = []
+            self.children_dict = {}
+            self.childrenNode = NodePath(name + "_children")
+            self.childrenNode.reparentTo(
+                aspect2d
+                if parent is None
+                else (
+                    parent.childrenNode
+                    if isinstance(parent, UIManager.Window)
+                    else parent
+                )
+            )
+
+            self.root = NodePath(name)
+            self.root.reparentTo(
+                aspect2d
+                if parent is None
+                else (
+                    parent.childrenNode
+                    if isinstance(parent, UIManager.Window)
+                    else parent
+                )
+            )
+            if parent is not None:
+                parent.children.append(self)
+                parent.children_dict[name] = self
+            self.hide()
+            UIMgr.addPage(pageName=self.name, page=self)
+
+        def show(self):
+            self.visible = True
+            self.root.show()
+
+        def hide(self):
+            self.visible = False
+            self.root.hide()
+
+        def showParent(self):
+            if not self.parent is None:
+                self.parent.show()
+
+        def hideParent(self):
+            if not self.parent is None:
+                self.parent.hide()
+
+        def destroy(self):
+            for child in self.children:
+                child.removeNode()
+            for index, child in enumerate(self.children_dict):
+                child.removeNode
+            self.children.clear()
+            self.children_dict.clear()
+            self.root.removeNode()
+            self.childrenNode.removeNode()
+            if self.parent is not None:
+                self.parent.children.remove(self)
+            self.visible = False
+            self.parent = None
+            del self
+
+        def appendWindow(self, parentWindow: "UIManager.Window"):
+            parentWindow.children.append(self)
+            parentWindow.children_dict[self.name] = self
+
+
+UIManager = UIManager()
+
+
+class _state:
+    PASS = "PASS"
+    FAIL = "FAIL"
+    ERR = "ERR"
+    MEM_INVALID = "MEM_INVALID"
+    VAL_INVALID = "VAL_INVALID"
+    ACCESS_DENIED = "ACCESS_DENIED"
+    OTHER = "OTHER"
+
+
+_state = _state()
+
+
+class FILEMGR:
+    def __init__(self):
+        self.keyStore = {"readmode": "r", "writemode": "w"}
+
+    def loadPrefs(self):
+        with open("./HYBERFIL", self.keyStore["readmode"]) as f:
+            global GLOBALMEM
+            try:
+                _f: dict = loads(f.read())
+                for d, v in _f.items():
+                    GLOBALMEM[d] = v
+            except Exception as e:
+                print("Error loading RAM disk: ", e)
+                GLOBALMEM.clear()
+                GLOBALMEM = DEFAULTS
+
+        print("Loaded RAM disk")
+
+    def savePrefs(self):
+        with open("./HYBERFIL", self.keyStore["writemode"]) as f:
+            dump(GLOBALMEM, f, indent=4)
+        print("Saved RAM disk")
+
+    def getKey(self, key):
+        return self.keyStore.get(key, None)
+
+    def setKey(self, key, value):
+        self.keyStore[key] = value
+
+
+FILEMGR = FILEMGR()
+
+
+class AUTH:
+    def login(self):
+        state = self.verify(self.userNameEntry.get(), self.passwordEntry.get())
+        if state is _state.PASS:
+            print("Logged in")
+        elif state is _state.VAL_INVALID:
+            print("Invalid Password")
+        elif state is _state.MEM_INVALID:
+            print("Invalid Username")
+        else:
+            print("Unknown Error")
+
+    def verify(self, username, password) -> _state:
+        if username in GLOBALMEM["AUTH"]["users"]:
+            if GLOBALMEM["AUTH"]["users"][username] == password:
+                return _state.PASS
+            else:
+                return _state.VAL_INVALID
+        elif len(username) == 0:
+            return _state.ERR
+        elif len(enumerate(GLOBALMEM["AUTH"]["users"])) == 0:
+            return _state.PASS
+        else:
+            return _state.MEM_INVALID
+
+
+AUTH = AUTH()
+
+
+class GUI:
+    def setTimeNodes(self, task):
+        self.lockScreenTimeNode.setText(time.strftime("%I:%M:%S"))
+        self.lockScreenDateNode.setText(time.strftime("%A, %B %Y"))
+        return task.cont
+
+    def __init__(self, base: "OS"):
+        self.base = base
+        self.base.setBackgroundColor(0, 0, 0, 1)
+        self.windows = []
+        self.lockScreenWindow = UIManager.Window("lockScreen", UIManager)
+        self.loginWindow = UIManager.Window("login", UIManager)
+
+        self.win11Font = self.base.loader.loadFont(
+            "./src/fonts/SegoeUIVF.ttf",
+            pixelsPerUnit=200,
+        )
+        self.lockScreenBackgroundImage = OnscreenImage(
+            image="./src/img/lockBackground.jpg",
+            scale=(1 * (1920 / 1080), 1, 1),
+            parent=self.lockScreenWindow.root,
+        )
+        self.lockScreenTimeNode = OnscreenText(
+            text=time.strftime("%I:%M:%S"),
+            font=self.win11Font,
+            fg=(1, 1, 1, 1),
+            pos=(0, 0.45),
+            scale=0.2,
+            mayChange=True,
+            parent=self.lockScreenWindow.root,
+        )
+        self.lockScreenDateNode = OnscreenText(
+            text=time.strftime("%A, %B %Y"),
+            font=self.win11Font,
+            fg=(1, 1, 1, 1),
+            pos=(0, 0.35),
+            scale=0.045,
+            mayChange=True,
+            parent=self.lockScreenWindow.root,
+        )
+        self.lockScreenWifiImage = OnscreenImage(
+            image="./src/img/wifi.png",
+            scale=(0.035 * (1280 / 942), 0.035, 0.035),
+            pos=(1, 0, -0.9),
+            parent=self.lockScreenWindow.root,
+        )
+        self.lockScreenWifiImage.setTransparency(TransparencyAttrib.MAlpha)
+
+        self.lockScreenWindow.show()
+        base.taskMgr.add(self.setTimeNodes, "setTimeNodes", delay=1)
+
+
+class OS(ShowBase):
+    def __init__(self):
+        super().__init__()
+        self.gui = GUI(self)
+
+
+FILEMGR.loadPrefs()
+
+loadPrcFileData("", GLOBALMEM["PANDAPRC"]["PRCFILE"])
+exec(GLOBALMEM["PANDAPRC"]["STARTUP_INJECTOR"])
+
+
+def exit_handler():
+    FILEMGR.savePrefs()
+
+
+atexit.register(exit_handler)
+
+base = OS()
+base.run()
